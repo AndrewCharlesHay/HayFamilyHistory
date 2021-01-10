424dcfd9e2c7348b138805101a65dd7f7f4abc20
AndrewCharlesHay <hay.149@osu.edu>
2021-01-10 17:15:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 
 var svg = d3.select("body").append("svg")
-  .attr("width", 960)
-  .attr("height", 1160)
+  .attr("width", 960*2)
+  .attr("height", 1160*2)
+ d3.json("places.geojson", function(error, data) {
+   let features = [];
+   var millisecondsToWait = 500;
+   setTimeout(function() {
+    for(feature in data.features){
+      features.push(feature)
+      var group = svg.selectAll("g")
+        .data(data.features)
+        .enter()
+        .append("g")
 
- d3.json("usa.geojson", function(error, data) {
-  var group = svg.selectAll("g")
-    .data(data.features)
-    .enter()
-    .append("g")
+      var projection = d3.geo.mercator(); //.scale(7300).translate([0,1980]);
+      var path = d3.geo.path().projection(projection);
 
-  var projection = d3.geo.mercator(); //.scale(7300).translate([0,1980]);
-  var path = d3.geo.path().projection(projection);
-
-  var areas = group.append("path")
-    .attr("d", path)
-    .attr("class", "area")
-    .attr("fill", "steelblue")
-
+      var areas = group.append("path")
+        .attr("d", path)
+        .attr("class", "area")
+        .attr("fill", "steelblue")
+    }
+  }, millisecondsToWait);
 });
