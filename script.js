222b4163330fd1c54169ebfe43bf02b0e170b878
AndrewCharlesHay <hay.149@osu.edu>
2021-01-10 17:37:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,22 +4,22 @@ var svg = d3.select("body").append("svg")
   .attr("height", 1160*2)
  d3.json("places.geojson", function(error, data) {
    let features = [];
-   var millisecondsToWait = 500;
-   for(feature in data.features){
+   const millisecondsToWait = 500;
+   for(let i = 0; i < data.features.length(); i++){
     setTimeout(function() {
-      features.push(feature)
+      features.push(data.features[i])
       var group = svg.selectAll("g")
         .data(features)
         .enter()
-        .append("g")
+        .append("g");
 
-      var projection = d3.geo.mercator(); //.scale(7300).translate([0,1980]);
+      var projection = d3.geo.mercator();
       var path = d3.geo.path().projection(projection);
 
       var areas = group.append("path")
         .attr("d", path)
         .attr("class", "area")
-        .attr("fill", "steelblue")
+        .attr("fill", "steelblue");
     }, millisecondsToWait);
-   }
+   };
 });
