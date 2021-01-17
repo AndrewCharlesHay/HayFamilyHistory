de67a5eca0d4a2c44c2f03bc9e0365a90a3e6a8a
AndrewCharlesHay <hay.149@osu.edu>
2021-01-16 22:02:56 -0500

    formatting

--- script.js
+++ script.js
@@ -5,28 +5,20 @@ const svg = d3.select("body").append("svg")
   .attr("width", map.width)
   .attr("height", map.height);
 const card = createCard();
+
 d3.json("places.geojson", async function(error, data) {
    for(let i = 0; i < data.features.length; i++){
-    features.push(data.features[i])
-    var group = svg.selectAll("g")
-      .data(features)
-      .enter()
-      .append("g");
-
-    var projection = d3.geo.mercator();
-    var path = d3.geo.path().projection(projection);
-
-    var areas = group.append("path")
-      .attr("d", path)
-      .attr("class", "area")
-      .attr("fill", "steelblue");
+    features.push(data.features[i]);
+    updateMap(features, svg);
     updateCard(data.features[i], card);
     await sleep(MILLISECONDS_TO_WAIT);
    };
-  });
+});
+
 async function sleep(msec) {
     return new Promise(resolve => setTimeout(resolve, msec));
 }
+
 function createCard() {
   const card = d3.select("body").append("div")
     .attr("class", "card")
@@ -41,6 +33,35 @@ function createCard() {
     .attr("class", "card-text");
   return { card, cardBody, header, paragraph, image };
 }
+
+function updateMap(features, svg){
+  const group = svg.selectAll("g")
+    .data(features)
+    .enter()
+    .append("g");
+  const projection = d3.geo.mercator();
+  const path = d3.geo.path().projection(projection);
+
+  const areas = group.append("path")
+    .attr("d", path)
+    .attr("class", "area")
+    .attr("fill", "steelblue");
+}
+
+function updateCard(feature, card){
+  const img = feature.img;
+  if(img){
+    card.image.attr("src", img);
+  }
+  const name = feature.name;
+  if(name){
+    card.header.text(name);
+  }
+  const paragraph = createParagraph(feature);
+  card.paragraph.text(paragraph);
+}
+
+
 function createParagraph(data) {
  if(!data.event || !data.dateOfEvent || !data.location){
   return "";
@@ -49,16 +70,4 @@ function createParagraph(data) {
   return `${data.event}: ${data.dateOfEvent}
 ${data.location}`;
  }
-}
-function updateCard(feature, card){
-   const img = feature.img;
-   if(img){
-     card.image.attr("src", img);
-   }
-   const name = feature.name;
-   if(name){
-     card.header.text(name);
-   }
-   const paragraph = createParagraph(feature);
-   card.paragraph.text(paragraph);
 }
