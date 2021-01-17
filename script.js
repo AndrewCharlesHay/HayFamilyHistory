2af91b50cba7a5d227f1d4d79a97e86a52bd95fa
AndrewCharlesHay <hay.149@osu.edu>
2021-01-17 17:12:32 -0500

    Changed the dimensions on sizeChange()

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ d3.json("places.geojson", async function(error, data) {
 });
 
 function sizeChange() {
-  d3.selectAll("g").attr("transform", "scale(" + $("body").width()/400 + ")");
-	$("svg").height($("body").width()*0.3);
+  d3.selectAll("g").attr("transform", "scale(" + $("body").width()/900 + ")");
+	$("svg").height($("body").width()*0.61);
 }
 
 async function sleep(msec) {
