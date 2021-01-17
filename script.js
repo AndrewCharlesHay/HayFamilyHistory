00a52b5be152d0c76cbabf4ed42289e6b54437e5
AndrewCharlesHay <hay.149@osu.edu>
2021-01-17 13:20:30 -0500

    Changed Height to .4

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ d3.json("places.geojson", async function(error, data) {
 
 function sizeChange() {
   d3.select("g").attr("transform", "scale(" + $("body").width()/900 + ")");
-	$("svg").height($("body").width()*0.618);
+	$("svg").height($("body").width()*0.4);
 }
 
 async function sleep(msec) {
