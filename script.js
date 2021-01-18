4c13ff05f11d53717f20630ccc5ecaed6676140d
AndrewCharlesHay <hay.149@osu.edu>
2021-01-18 13:56:54 -0500

    Added stock photo

--- script.js
+++ script.js
@@ -59,9 +59,10 @@ function updateMap(features, svg){
 
 function updateCard(feature, card){
   const img = feature.img;
-  if(img){
-    card.image.attr("src", img);
+  if(!img){
+	  img = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
   }
+  card.image.attr("src", img);
   const name = feature.name;
   if(name){
     card.title.text(name);
