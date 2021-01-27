f2c753492d2f8e12cdbd0d83278d859019c04002
AndrewCharlesHay <hay.149@osu.edu>
2021-01-26 20:28:04 -0500

    commented out initial map

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ const svg = d3.select("body")
 	.append("svg");
 const card = createCard(); 
 d3.select(window).on("resize", sizeChange);
-d3.json("places.geojson", (error, data) => {
-	for(let i = 0; i < data.features.length; i++){
-	   features.push(data.features[i]);
-	   updateMap(features, svg);
-	   sizeChange();
-	};
-});
+// d3.json("places.geojson", (error, data) => {
+// 	for(let i = 0; i < data.features.length; i++){
+// 	   features.push(data.features[i]);
+// 	   updateMap(features, svg);
+// 	   sizeChange();
+// 	};
+// });
 
 d3.json("events.geojson", async function(error, data) {
 	for(let i = 0; i < data.features.length; i++){
